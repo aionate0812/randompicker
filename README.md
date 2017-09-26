56532a957570538500508577aa9a6ecbe08f6de8
dinnall <michaeldinnall>
2017-09-26 08:38:27 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+### Random Student Picker for PTech 2017-2018
+
+
+Application to encourage participation. 
+
+
+
+## Coming soon
+
+* Add a form to dynamically add names 
+* Ability to add any given gif/picture
+* Have users add their own title
+* User Modification of CSS ?
+
+
+
---
+++ rand.js
@@ -0,0 +1,130 @@
+var pickButton;
+var pairButton;
+var aramButton;
+var header;
+var body;
+var choice;
+var titles;
+
+var students = [
+  "Aaron",
+  "Cedrick",
+  "Cindi",
+  "Ethan",
+  "George",
+  "Grey",
+  "Ike",
+  "Jahmaali",
+  "Jai'lyn",
+  "Jalanie",
+  "Jason",
+  "Jeremy",
+  "Joel",
+  "Jordin",
+  "Joseph",
+  "Joshua",
+  "Josiah",
+  "Kory",
+  "Lamar",
+  "Mario",
+  "Nathan",
+  "Tyquan",
+  "Tyreke",
+  "Kyle",
+  "Julien"
+];
+
+var titles = [
+  "The Just",
+  "The Magnificent",
+  "The Magnanimous",
+  "The Knower of Things",
+  "The The Golden Hearted",
+  "The Clever",
+  "The Great Wit",
+  "The Kind",
+  "The Caring",
+  "The Jovial",
+  "The Courageous",
+  "Whose Friendship is Magic",
+  "Lover of Trail Mix",
+  "The Caffeinated",
+  "Lover of Brooklyn",
+  "The Whisperer",
+];
+
+
+var colors = [
+  '#cf5d47',
+  '#ffed00',
+  '#800000',
+  '#f7e100',
+  '##500000',
+  '#efed00',
+  '#780000 ',
+  '#f5d245',
+  '#480000',
+  '#fbdd45',
+  '#880000 ',
+];
+
+
+
+var cur = 0;
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('hello rand.js');
+
+  body = document.getElementById('body');
+  header = document.getElementById('header');
+  pickButton = document.getElementById('pickButton');
+  pairButton = document.getElementById('pairButton');  
+  choice = document.getElementById('choice');
+
+  pickButton.addEventListener('click', pickOnClick );
+  pairButton.addEventListener('click', pairClick );
+});
+
+
+
+var pickOnClick = function (event) {
+  choice.innerHTML = '&nbsp;'
+  var rand = students.splice(Math.floor(Math.random() * students.length),1); 
+  var title = titles.splice(Math.floor(Math.random() * titles.length), 1);
+  var x = window.setInterval(() => {
+    if (colors[cur] === undefined) {
+      body.style.backgroundColor = 'goldenrod;'
+      window.clearInterval(x);
+      cur = 0;
+      if (rand[0].length % 2 === 0) {
+        rand[0] = rand[0] 
+      }
+      choice.innerText = (rand[0] + ' ' + title).toUpperCase() + '!'
+    
+      // return;
+      
+    }  
+    if (colors[cur]) header.style.color = colors[cur];
+    if (colors[cur-1]) body.style.backgroundColor = colors[cur-1];
+    cur += 1;
+  }, 200);
+}
+
+
+var pairClick = function (event) {
+  choice.innerHTML = '&nbsp;'
+  var rand = students.splice(Math.floor(Math.random() * students.length),1); 
+  var randTwo = students.splice(Math.floor(Math.random() * students.length),1);
+
+  var x = window.setInterval(() => {
+    if (colors[cur] === undefined) {
+      window.clearInterval(x);
+      cur = 0;
+      choice.innerHTML = rand + ' &#x2764; ' + randTwo
+      // return;  
+    }  
+    if (colors[cur]) header.style.color = colors[cur];
+    if (colors[cur-1]) body.style.backgroundColor = colors[cur-1];
+    cur += 1;
+  }, 200);
+}
